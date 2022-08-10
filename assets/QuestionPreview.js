19321f5a15cee98923fd74e792062dfc919a39c9
joseph <personalwcj0715@gmail.com>
2022-08-10 10:49:03 -0500

    More CSS changes to use dark mode

--- assets/QuestionPreview.js
+++ assets/QuestionPreview.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function QuestionPreview({first_question,group_title}){
 
     return(
-        <div>
+        <div className="card">
             <h2>
                 {group_title}
             </h2>
