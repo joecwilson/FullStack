6a928e09f64075c4ff35f267b8414a8314dee981
joseph <personalwcj0715@gmail.com>
2022-08-12 11:19:46 -0500

    Added description field to question list

--- question_list/models.py
+++ question_list/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class QuestionList(models.Model):
     title = models.CharField(max_length=1500)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
