f20b8579514fb9dc8509212aac401cd0b189daf1
joseph <personalwcj0715@gmail.com>
2022-08-12 11:43:06 -0500

    Updated character lengths

--- question_list/models.py
+++ question_list/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class QuestionList(models.Model):
-    title = models.CharField(max_length=1500)
-    description = models.TextField()
+    title = models.CharField(max_length=300)
+    description = models.CharField(max_length=1500)
 
     def __str__(self):
         return self.title
