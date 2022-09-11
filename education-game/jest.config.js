80897128f7f8c364e77af81187a5bae3c25718c3
joseph <personalwcj0715@gmail.com>
2022-09-11 11:32:01 -0500

    Deleted Tailwind, and added basic test

---
+++ education-game/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rootDir: './education-game',
+    testMatch: [
+        '<rootDir>/education-game/src/__tests__/*.test.js',
+        '<rootDir>/education-game/src/__tests__/*.test.jsx',
+    ],
+    roots: [
+        '<rootDir>/education-game/src/__tests__/*.test.js'
+    ],
+
+}
