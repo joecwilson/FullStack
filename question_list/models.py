e438317fa22b4baeb21955b0b9707f435925e312
joseph <personalwcj0715@gmail.com>
2022-09-25 14:38:51 -0500

    Added Creation_date to questionLists

--- question_list/models.py
+++ question_list/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class QuestionList(models.Model):
     title = models.CharField(max_length=300)
     description = models.CharField(max_length=1500)
+    creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
