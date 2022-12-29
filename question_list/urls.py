e28e1f8e57f06ddb39fd1751e85ca76c398f5d8a
joseph <josephcwilson47@gmail.com>
2022-12-29 14:27:16 -0600

    users can make new question groups

--- question_list/urls.py
+++ question_list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import QuestionGroupView
+from .views import QuestionGroupView, QuestionListCreateView
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path("<int:pk>/", TemplateView.as_view(template_name='question_list.html'), name="react"),
+    path("create_new/", QuestionListCreateView.as_view(), name="new question"),
     path("", QuestionGroupView.as_view(), name="home"),
 
 ]
--- question_list/views.py
+++ question_list/views.py
@@ -2,7 +2,9 @@ from django.views.generic import ListView
 from .models import QuestionList
 from rest_framework import viewsets, generics
 from .serializers import QuestionListSerializer
-
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 
 class QuestionGroupView(ListView):
     model = QuestionList
@@ -17,3 +19,16 @@ class ListQuestionList(generics.ListAPIView):
 class DetailQuestionList(generics.RetrieveAPIView):
     serializer_class = QuestionListSerializer
     queryset = QuestionList.objects.all()
+
+
+class QuestionListCreateView(LoginRequiredMixin, CreateView):
+    model = QuestionList
+    template_name = 'create_questions.html'
+    fields = ['title', 'description' ]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('profile')
