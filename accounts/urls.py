5c66fa00a570e5cd6dee8dd4f7679805b9e96406
joseph <josephcwilson47@gmail.com>
2022-12-30 12:53:04 -0600

    added namespaces

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import SignUpView, ProfileView
 
+app_name = 'account'
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("profile/", ProfileView.as_view(), name="profile"),
--- question_list/urls.py
+++ question_list/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import QuestionGroupView, QuestionListCreateView
 from django.views.generic import TemplateView
 
+app_name = 'question_list'
 urlpatterns = [
     path("<int:pk>/", TemplateView.as_view(template_name='question_list.html'), name="react"),
-    path("create_new/", QuestionListCreateView.as_view(), name="new question"),
+    path("create_new/", QuestionListCreateView.as_view(), name="new group"),
     path("", QuestionGroupView.as_view(), name="home"),
 
 ]
